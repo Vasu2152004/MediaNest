1b7f79c81bd48b02cf88a96a88b2d664090a98ec
vasu <vkkalavadiya21@gmail.com>
2025-04-07 11:11:06 +0530

    some changes in DB connections

--- src/index.js
+++ src/index.js
@@ -6,3 +6,15 @@ dotenv.config({
 }
 )
 connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error Occurs while listening app ",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Serever is Running on Port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log(`Error Occurs in MONGO Conn  ${err}`)
+})
