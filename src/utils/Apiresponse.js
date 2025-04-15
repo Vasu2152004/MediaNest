ae267c06dd80dbadf1bb427306feb71717755b74
vasu <vkkalavadiya21@gmail.com>
2025-04-15 18:16:56 +0530

    Utils code

---
+++ src/utils/Apiresponse.js
@@ -0,0 +1,8 @@
+class Apiresponse{
+    constructor(stauscode,data,message="Success"){
+        this.stauscode=stauscode,
+        this.data=data,
+        this.message=message,
+        this.success=stauscode
+    }
+}
